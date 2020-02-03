ccf3b6d9271815b92d655c8eeed200506f3cc871
Nick Steinhauser <nicksteinhauser@gmail.com>
2020-02-03 09:16:34 -0600

    Update criteria-check.js

--- modified-functions/criteria-check.js
+++ modified-functions/criteria-check.js
@@ -7,45 +7,43 @@
   * @return The output of this action, which must be a JSON object.
   *
   */
-function main(params) {
-    const VALID_REPOS = [ 
-                        'nfstein/git-sandbox',
-                        'IBM/mean-app',
-                        'IBM/mern-app',
-                        'IBM/swift-kitura-app',
-                        'IBM/swift-kitura-hyper-protect-dbaas',
-                        'IBM/node-red-app',
-                        'IBM/nodejs-express-app',
-                        'IBM/nodejs-cloudant',
-                        'IBM/java-liberty-app',
-                        'IBM/java-spring-app',
-                        'IBM/python-flask-app',
-                        'IBM/python-django-app',
-                        'IBM/go-gin-app',
-                        'IBM/knative-eventing-java-app',
-                        'watson-developer-cloud/natural-language-understanding-code-pattern',
-                        'watson-developer-cloud/speech-to-text-code-pattern',
-                        'watson-developer-cloud/visual-recognition-code-pattern',
-                        'watson-developer-cloud/text-to-speech-code-pattern'
-                    ]
+const VALID_REPOS = [ 
+                    'nfstein/git-sandbox',
+                    'IBM/mean-app',
+                    'IBM/mern-app',
+                    'IBM/swift-kitura-app',
+                    'IBM/swift-kitura-hyper-protect-dbaas',
+                    'IBM/node-red-app',
+                    'IBM/nodejs-express-app',
+                    'IBM/nodejs-cloudant',
+                    'IBM/java-liberty-app',
+                    'IBM/java-spring-app',
+                    'IBM/python-flask-app',
+                    'IBM/python-django-app',
+                    'IBM/go-gin-app',
+                    'IBM/knative-eventing-java-app',
+                    'watson-developer-cloud/natural-language-understanding-code-pattern',
+                    'watson-developer-cloud/speech-to-text-code-pattern',
+                    'watson-developer-cloud/visual-recognition-code-pattern',
+                    'watson-developer-cloud/text-to-speech-code-pattern'
+                ]
 
-    const master = 'refs/heads/master'
-    const development = 'refs/heads/development'
-    
-    // repo name and urls
-    const repoFullName = params.repository.full_name
-      
-    console.log("Registration request recieved for pattern: " + repoFullName)
-    
-    
-    // Early-out conditions - invalid repos & non-master branches
-    if (!VALID_REPOS.includes(repoFullName)) {
-        return { error: 'Not a valid repository: ' + repoFullName }
-    }
-    if (params.ref !== master && params.ref !== development) {
-        console.log("Ref: "+ params.ref)
-        return { error: 'Not a change to master - exiting registration' }
-    }
-    
-    return params
+const master = 'refs/heads/master'
+const development = 'refs/heads/development'
+
+// repo name and urls
+const repoFullName = params.repository.full_name
+
+console.log("Registration request recieved for pattern: " + repoFullName)
+
+
+// Early-out conditions - invalid repos & non-master branches
+if (!VALID_REPOS.includes(repoFullName)) {
+    return { error: 'Not a valid repository: ' + repoFullName }
 }
+if (params.ref !== master && params.ref !== development) {
+    console.log("Ref: "+ params.ref)
+    return { error: 'Not a change to master - exiting registration' }
+}
+
+return params
